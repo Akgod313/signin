d751f36790d8f933930b3c76bf17be76c2761ede
Akgod313 <ayaankarkera8@gmail.com>
2025-12-18 17:47:53 +0530

    Add files via upload

---
+++ signin/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { auth } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
+import styles from "./index.module.css";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <main
+          ></main>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
